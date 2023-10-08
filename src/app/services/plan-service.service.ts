686bc29dd1e9d3c126af02ea0aa560fa9c429294
Dawid <ircurlicus123@gmail.com>
2023-10-08 14:27:56 +0200

    Changed url for prod env

--- src/app/services/plan-service.service.ts
+++ src/app/services/plan-service.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import {Injectable, isDevMode} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {PlanItem} from "../models/PlanItem";
@@ -10,7 +10,8 @@ export class PlanServiceService {
   constructor(private httpClient: HttpClient) { }
 
   getPlanForStudent(indexNumber: string, startDate: Date, endDate: Date): Observable<PlanItem[]> {
-    const url = `/api/schedule_student.php?start=${startDate}&end=${endDate}&number=${indexNumber}`;
+    const baseUrl = isDevMode() ? '/api' : 'https://plan.zut.edu.pl';
+    const url = `${baseUrl}/schedule_student.php?start=${startDate}&end=${endDate}&number=${indexNumber}`;
     return this.httpClient.get<PlanItem[]>(url);
   }
 }
